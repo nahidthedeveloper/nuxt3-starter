674e1c0af63438c021013d664588a5c29e79c9cc
Nahid <nahidthedeveloper@gmail.com>
2024-08-18 23:18:43 +0600

    update layout loginForm and error page

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -21,8 +21,8 @@ const handleSignIn = async () => {
 <template>
   <form class="space-y-4 md:space-y-6" action="#" @submit.prevent="handleSignIn">
     <div>
-      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-      <input type="text" id="email" placeholder="name@company.com" required
+      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email or Username</label>
+      <input type="text" id="email" placeholder="Email or Username" required
              v-model="form.email_or_username"
              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="my-12">
+  <div class="my-12 max-w-screen-xl mx-auto">
     <slot/>
   </div>
 </template>
